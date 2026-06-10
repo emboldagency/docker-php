90f6c8e96557374fc2fcd69e728a15f637a414bb
tyler <tyler@embold.com>
2026-06-09 19:18:43 -0700

    :arrow_up: Bump Vault CLI to 2.0.2 now that binaries are released

--- main.tf
+++ main.tf
@@ -546,7 +546,7 @@ module "vault" {
   agent_id   = coder_agent.main.id
   vault_addr = "https://vault.embold.dev"
   # Pin: before bumping, verify binaries exist at https://releases.hashicorp.com/vault/<version>/.
-  vault_cli_version = "2.0.0"
+  vault_cli_version = "2.0.2"
 }
 
 # DEPRECATED: Keep these parameters for backward compatibility with workspaces
